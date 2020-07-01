4ce669d4e692e1ab35cca1a702c9de2bb17bcb5d
JiaqiJin96 <fdsam@hotmail.com>
2020-07-01 15:16:11 +0200

    direction

--- Core/Core/core/header.h
+++ Core/Core/core/header.h
@@ -1,6 +1,15 @@
 #ifndef header_hpp
 #define header_hpp
 
+template<typename T>
+class Point2;
+
+template<typename T>
+class Point3;
+
+template<typename T>
+class Normal3;
+
 #include<iostream>
 #include<cmath>
 #include "../util/macro.hpp"
