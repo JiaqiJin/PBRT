a9e0cb2bb60e3536da8cdf00c6a86839cedba80d
JiaqiJin96 <fdsam@hotmail.com>
2020-07-24 11:52:13 +0200

    animate trans

--- Core/Core/math/animatedtransform.hpp
+++ Core/Core/math/animatedtransform.hpp
@@ -10,32 +10,43 @@ KAWAII_BEGIN
 
 class AnimatedTransform {
 
-public:
     AnimatedTransform(const Transform* startTransform, Float startTime,
         const Transform* endTransform, Float endTime);
-
-    static void Decompose(const Matrix4x4& m, Vector3f* T, Quaternion* R,
+    /*
+     基本思路
+     1.直接提取出平移变换
+     2.利用旋转矩阵为正交矩阵的性质(正交矩阵的逆与转置相等)对剔除了平移变换的矩阵进行极分解
+     极分解的思路如下:
+     M = TRS
+     Mi+1 = 1/2(Mi + inverse(transpose(Mi)))
+     重复上诉操作直到Mi收敛到一个固定的值，这个值就是旋转矩阵R
+     M = RS
+     S = inverse(R) * M
+     */
+    static void decompose(const Matrix4x4& m, Vector3f* T, Quaternion* R,
         Matrix4x4* S);
 
-    void Interpolate(Float time, Transform* t) const;
+    void interpolate(Float time, Transform* t) const;
 
-    Ray operator()(const Ray& r) const;
+    Ray exec(const Ray& r) const;
 
-    RayDifferential operator()(const RayDifferential& r) const;
+    RayDifferential exec(const RayDifferential& r) const;
 
-    Point3f operator()(Float time, const Point3f& p) const;
+    Point3f exec(Float time, const Point3f& p) const;
 
-    Vector3f operator()(Float time, const Vector3f& v) const;
+    Vector3f exec(Float time, const Vector3f& v) const;
 
-    bool HasScale() const {
+    bool hasScale() const {
         return _startTransform->hasScale() || _endTransform->hasScale();
     }
 
     Bounds3f MotionBounds(const Bounds3f& b) const;
 
     Bounds3f BoundPointMotion(const Point3f& p) const;
+public:
 
 private:
+
     const Transform* _startTransform;
     const Transform* _endTransform;
     const Float _startTime;
