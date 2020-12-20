683cb6a09f385eb92c494987dbaab48a790c18cc
kawakawaDe <fdsam@hotmail.com>
2020-12-20 13:06:00 +0000

    filter

--- Paladin/Paladin/filters/box.h
+++ Paladin/Paladin/filters/box.h
@@ -1,6 +1,28 @@
-#ifndef box_hpp
+﻿#ifndef box_hpp
 #define box_hpp
 
-#include <stdio.h>
+#include "filter.h"
 
-#endif /* box_hpp */
+PALADIN_BEGIN
+
+/**
+ * 盒式滤波器，效果最差的滤波器
+ * 频域下为sinc函数
+ */
+class BoxFilter : public Filter
+{
+public:
+	BoxFilter(const Vector2f& radius) : Filter(radius){
+	}
+
+    /**
+     * 由于外层传入值的时候已经做了范围校验，可以直接返回1
+     */
+    virtual Float evaluate(const Point2f& p) const {
+        return 1.0f;
+    }
+};
+
+PALADIN_END
+
+#endif
--- Paladin/Paladin/filters/triangle.h
+++ Paladin/Paladin/filters/triangle.h
@@ -1,1 +1,22 @@
-#pragma once
+#ifndef filters_triangle_h
+#define filters_triangle_h
+
+#include "filter.h"
+
+PALADIN_BEGIN
+
+class TriangleFilter : public Filter
+{
+public:
+    TriangleFilter(const Vector2f& radius) : Filter(radius) {
+    }
+
+    virtual Float evaluate(const Point2f& p) const {
+        return std::max((Float)0, radius.x - std::abs(p.x)) *
+            std::max((Float)0, radius.y - std::abs(p.y));
+    }
+};
+
+PALADIN_END
+
+#endif 
