2be1aabd345da45a34f54649ad0b16c90d3f68fb
kawakawaDe <fdsam@hotmail.com>
2021-01-08 12:09:39 +0000

    lambertian model

--- Paladin/Paladin/core/bxdf.hpp
+++ Paladin/Paladin/core/bxdf.hpp
@@ -768,6 +768,94 @@ private:
     const TransportMode _mode;
 };
 
+/*
+* 朗伯反射又称理想漫反射
+ * 半空间上各个方向的反射率相同
+ * 理想漫反射在物理上是不可能的
+ * 
+ * 由能量守恒得 ∫fr(p,wo,wi)|cosθi|dwi = 1
+ * 
+ * 
+*/
+class LambertianReflection : public BxDF
+{
+public:
+    LambertianReflection(const Spectrum& R)
+        :BxDF(BxDFType(BSDF_REFLECTION | BSDF_DIFFUSE)),
+        _R(R) {}
+
+    // 朗伯反射中任何方向的反射率都相等
+    virtual Spectrum f(const Vector3f& wo, const Vector3f& wi) const {
+        return _R * InvPi;
+    }
+
+    // 朗伯反射中任何方向的反射率都相等
+    virtual Spectrum rho_hd(const Vector3f&, int, const Point2f*) const {
+        return _R;
+    }
+
+    // 朗伯反射中任何方向的反射率都相等
+    virtual Spectrum rho_hh(int, const Point2f*, const Point2f*) const {
+        return _R;
+    }
+
+    virtual std::string toString() const {
+        return std::string("[ LambertianReflection R: ") + _R.ToString() +
+            std::string(" ]");
+    }
+
+private:
+    const Spectrum _R;
+};
+
+/**
+ * 朗伯散射
+ * 原理跟朗伯反射相同，具体推导过程参见朗伯反射
+ * 不再赘述
+ */
+class LambertianTransmission : public BxDF 
+{
+public:
+    LambertianTransmission(const Spectrum& T)
+        : BxDF(BxDFType(BSDF_TRANSMISSION | BSDF_DIFFUSE)),
+        _T(T) {}
+
+    virtual Spectrum f(const Vector3f& wo, const Vector3f& wi) const {
+        return _T * InvPi;
+    }
+
+    virtual Spectrum rho_hd(const Vector3f&, int, const Point2f*) const {
+        return _T;
+    }
+
+    virtual Spectrum rho_hh(int, const Point2f*, const Point2f*) const {
+        return _T;
+    }
+
+    virtual Spectrum sample_f(const Vector3f& wo, Vector3f* wi, const Point2f& u,
+        Float* pdf, BxDFType* sampledType) const {
+        *wi = cosineSampleHemisphere(u);
+        if (wo.z > 0) {
+            wi->z *= -1;
+        }
+        *pdf = pdfW(wo, *wi);
+        return f(wo, *wi);
+    }
+
+    virtual Float pdfW(const Vector3f& wo, const Vector3f& wi) const {
+        return sameHemisphere(wo, wi) ? 0 : absCosTheta(wi) * InvPi;
+    }
+
+    virtual std::string toString() const {
+        return std::string("[ LambertianTransmission T: ") + _T.ToString() +
+            std::string(" ]");
+    }
+
+private:
+    // 散射系数
+    Spectrum _T;
+};
+
 PALADIN_END
 
 #endif /* bxdf_hpp */
