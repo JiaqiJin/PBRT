594bc8e0e4906d82d9e5e182f377a8b2e3e1ce41
kawakawaDe <fdsam@hotmail.com>
2021-04-12 17:45:03 +0100

    phase function

--- EXCALIBUR/EXCALIBUR/core/Medium.h
+++ EXCALIBUR/EXCALIBUR/core/Medium.h
@@ -4,6 +4,45 @@
 
 RENDERING_BEGIN
 
+class PhaseFunction {
+public:
+    virtual ~PhaseFunction() {}
+    virtual Float p(const Vector3f& wo, const Vector3f& wi) const = 0;
+    virtual Float sample_p(const Vector3f& wo, Vector3f* wi,
+        const Point2f& u) const = 0;
+
+    virtual std::string toString() const = 0;
+};
+
+/*
+Henyey和Greenstein(1941)开发了一种广泛使用的相位函数。
+* 这种相位函数是专门设计的，以方便适合测量散射数据。 measured scattering data
+*/
+class HenyeyGreenstein : public PhaseFunction {
+public:
+    HenyeyGreenstein(Float g)
+        : _g(g) {}
+
+    virtual Float p(const Vector3f& wo, const Vector3f& wi) const override;
+
+    virtual Float sample_p(const Vector3f& wo, Vector3f* wi,
+        const Point2f& u) const override;
+
+    virtual std::string toString() const override {
+        return StringPrintf("[ HenyeyGreenstein g: %f ]", _g);
+    }
+private:
+    // 不对称参数 asymmetry parameter
+    const Float _g;
+};
+
+// Henyey和Greenstein开发的phase函数
+inline Float phaseHG(Float cosTheta, Float g) {
+    Float denom = 1 + g * g + 2 * g * cosTheta;
+    return Inv4Pi * (1 - g * g) / (denom * std::sqrt(denom));
+}
+
+// 介质
 class Medium {
 public:
     // Medium Interface
