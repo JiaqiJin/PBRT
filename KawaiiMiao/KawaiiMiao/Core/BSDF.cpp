3049f80d799d778d978137747ce0dadf5fba2826
kawakawaDe <fdsam@hotmail.com>
2021-04-30 17:27:06 +0100

    Reflection and refraction

--- KawaiiMiao/KawaiiMiao/Core/BSDF.cpp
+++ KawaiiMiao/KawaiiMiao/Core/BSDF.cpp
@@ -29,4 +29,86 @@ Float frDielectric(Float cosThetaI, Float etaI, Float etaT)
 	return (Rparl * Rparl + Rperp * Rperp) / 2;
 }
 
+// https://seblagarde.wordpress.com/2013/04/29/memo-on-fresnel-equations/
+Spectrum FrConductor(Float cosThetaI, const Spectrum& etaI, const Spectrum& etaT, const Spectrum& k)
+{
+	cosThetaI = clamp(cosThetaI, -1, 1);
+	Spectrum eta = etaT / etaI;
+	Spectrum etak = k / etaI;
+
+	Float cosThetaI2 = cosThetaI * cosThetaI;
+	Float sinThetaI2 = 1. - cosThetaI2;
+	Spectrum eta2 = eta * eta;
+	Spectrum etak2 = etak * etak;
+
+	Spectrum t0 = eta2 - etak2 - sinThetaI2;
+	Spectrum a2plusb2 = sqrt(t0 * t0 + 4 * eta2 * etak2);
+	Spectrum t1 = a2plusb2 + cosThetaI2;
+	Spectrum a = sqrt(0.5f * (a2plusb2 + t0));
+	Spectrum t2 = (Float)2 * cosThetaI * a;
+	Spectrum Rs = (t1 - t2) / (t1 + t2);
+
+	Spectrum t3 = cosThetaI2 * a2plusb2 + sinThetaI2 * sinThetaI2;
+	Spectrum t4 = t2 * sinThetaI2;
+	Spectrum Rp = Rs * (t3 - t4) / (t3 + t4);
+
+	return 0.5 * (Rp + Rs);
+}
+
+// BxDF
+
+Spectrum BxDF::sample_f(const Vector3f& wo, Vector3f& wi, const Vector2f& sample,
+	Float& pdf, BxDFType& sampledType) const
+{
+	// Cosine-sample the hemisphere, flipping the direction if necessary
+	wi = cosineSampleHemisphere(sample);
+	if (wo.z < 0)
+	{
+		wi.z *= -1;
+	}
+
+	pdf = this->pdf(wo, wi);
+
+	return f(wo, wi);
+}
+
+Float BxDF::pdf(const Vector3f& wo, const Vector3f& wi) const
+{
+	return sameHemisphere(wo, wi) ? glm::abs(wi.z) * InvPi : 0;
+}
+
+// Lambertian
+Spectrum LambertianReflection::f(const Vector3f& wo, const Vector3f& wi) const
+{
+	return m_R * InvPi;
+}
+
+// SpecularReflection 
+Spectrum SpecularReflection::sample_f(const Vector3f& wo, Vector3f& wi, const Vector2f& sample,
+	Float& pdf, BxDFType& sampledType) const
+{
+	wi = Vector3f(-wo.x, -wo.y, wo.z);
+	pdf = 1;
+	return m_Fresnel->evaluate(wi.z) * m_R / glm::abs(wi.z);
+}
+
+// SpecularTransmition
+Spectrum SpecularTransmission::sample_f(const Vector3f& wo, Vector3f& wi, const Vector2f& sample,
+	Float& pdf, BxDFType& sampledType) const
+{
+	bool entering = (wo.z) > 0;
+	Float etaI = entering ? m_etaA : m_etaB;
+	Float etaT = entering ? m_etaB : m_etaA;
+
+	if (!refract(wo, faceforward(Vector3f(0, 0, 1), wo), etaI / etaT, wi))
+		return 0;
+
+	pdf = 1;
+	Spectrum ft = m_T * (Spectrum(1.) - m_fresnel.evaluate(wi.z));
+	// Account for non-symmetry with transmission to different medium
+	if (m_mode == TransportMode::Radiance)
+		ft *= (etaI * etaI) / (etaT * etaT);
+	return ft / glm::abs(wi.z);
+}
+
 RENDER_END
--- KawaiiMiao/KawaiiMiao/Core/BSDF.h
+++ KawaiiMiao/KawaiiMiao/Core/BSDF.h
@@ -18,6 +18,7 @@ enum BxDFType
 };
 
 Float frDielectric(Float cosThetaI, Float etaI, Float etaT);
+Spectrum FrConductor(Float cosThetaI, const Spectrum& etaI, const Spectrum& etaT, const Spectrum& k);
 
 inline bool sameHemisphere(const Vector3f& w, const Vector3f& wp) { return w.z * wp.z > 0; }
 
@@ -28,27 +29,119 @@ class BSDF
 
 class BxDF
 {
-	
+public:
+	BxDF(BxDFType type) : m_type(type) {}
+
+	virtual ~BxDF() = default;
+
+	bool matchesFlags(BxDFType t) const { return (m_type & t) == m_type; }
+
+	virtual Spectrum f(const Vector3f& wo, const Vector3f& wi) const = 0;
+	virtual Spectrum sample_f(const Vector3f& wo, Vector3f& wi, const Vector2f& sample,
+		Float& pdf, BxDFType& sampledType) const;
+
+	virtual Float pdf(const Vector3f& wo, const Vector3f& wi) const;
+
+	// BxDF Public Data
+	const BxDFType m_type;
 };
 
+// Fresnel
 class Fresnel
 {
+public:
+	virtual ~Fresnel() = default;
+	virtual Spectrum evaluate(Float cosI) const = 0;
+};
 
+class FresnelDielectric : public Fresnel
+{
+public:
+	FresnelDielectric(Float etaI, Float etaT) : m_etaI(etaI), m_etaT(etaT) {}
+
+	virtual Spectrum evaluate(Float cosThetaI) const override
+	{
+		return frDielectric(cosThetaI, m_etaI, m_etaT);
+	}
+
+private:
+	Float m_etaI, m_etaT;
+};
+
+class FresnelConductor : public Fresnel {
+public:
+	FresnelConductor(const Spectrum& etaI, const Spectrum& etaT, const Spectrum& k)
+		: m_etaI(etaI), m_etaT(etaT), k(k) { }
+
+	virtual Spectrum evaluate(Float cosThetaI) const override
+	{
+		return FrConductor(std::abs(cosThetaI), m_etaI, m_etaT, k);
+	}
+private:
+	Spectrum m_etaI, m_etaT, k;
 };
 
 class FresnelNoOp : public Fresnel
 {
-
+public:
+	virtual Spectrum evaluate(Float) const override { return Spectrum(1.); }
 };
 
+// Reflections
 class LambertianReflection : public BxDF
 {
+public:
+	// LambertianReflection Public Methods
+	LambertianReflection(const Spectrum& R)
+		: BxDF(BxDFType(BSDF_REFLECTION | BSDF_DIFFUSE)), m_R(R) {}
 
+	virtual Spectrum f(const Vector3f& wo, const Vector3f& wi) const override;
+
+private:
+	// LambertianReflection Private Data
+	const Spectrum m_R;
 };
 
+class SpecularReflection : public BxDF
+{
+public:
+	SpecularReflection(const Spectrum& R, Fresnel* fresnel)
+		: BxDF(BxDFType(BSDF_REFLECTION | BSDF_SPECULAR)),
+		m_R(R), m_Fresnel(fresnel) {}
+
+	virtual Spectrum f(const Vector3f& wo, const Vector3f& wi) const override { return Spectrum(0.f); }
+
+	virtual Spectrum sample_f(const Vector3f& wo, Vector3f& wi, const Vector2f& sample,
+		Float& pdf, BxDFType& sampledType) const override;
+
+	virtual Float pdf(const Vector3f& wo, const Vector3f& wi) const override { return 0.f; }
+
+private:
+	const Spectrum m_R;
+	const Fresnel* m_Fresnel;
+};
+
+// Transmitions
 class SpecularTransmission : public BxDF
 {
+public:
+	// SpecularTransmission Public Methods
+	SpecularTransmission(const Spectrum& T, Float etaA, Float etaB, TransportMode mode)
+		: BxDF(BxDFType(BSDF_TRANSMISSION | BSDF_SPECULAR)), m_T(T), m_etaA(etaA),
+		m_etaB(etaB), m_fresnel(etaA, etaB), m_mode(mode) {}
 
+	virtual Spectrum f(const Vector3f& wo, const Vector3f& wi) const override { return Spectrum(0.f); }
+
+	virtual Spectrum sample_f(const Vector3f& wo, Vector3f& wi, const Vector2f& sample,
+		Float& pdf, BxDFType& sampledType) const override;
+
+	virtual Float pdf(const Vector3f& wo, const Vector3f& wi) const override { return 0.f; }
+
+private:
+	const Spectrum m_T;
+	const Float m_etaA, m_etaB;
+	const FresnelDielectric m_fresnel;
+	const TransportMode m_mode;
 };
 
 RENDER_END
